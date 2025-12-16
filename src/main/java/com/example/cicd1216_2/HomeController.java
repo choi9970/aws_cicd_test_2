872f682e346b943b898115d8dbdc23b3acc9761c
choi9970 <j.choi9970@gmail.com>
2025-12-16 11:25:59 +0900

    first commit2

---
+++ src/main/java/com/example/cicd1216_2/HomeController.java
@@ -0,0 +1,24 @@
+package com.example.cicd1216_2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+
+    @GetMapping("/hicicd")
+    public String hicicd(){
+        return "hicicd 1216^^";
+    }
+
+    @GetMapping("/hicicd2")
+    public String hicicd2(){
+        return "hicicd 1216-2^^";
+    }
+
+    @GetMapping("/hicicd3")
+    public String hicicd3(){
+        return "hicicd 1216-3^^";
+    }
+}
